b26fc9670286a42bddf52311bc27e1d989ea47ba
Team Neiman <136953008+NEIMAN-AI@users.noreply.github.com>
2023-09-11 12:42:24 +0530

    Create echo.py

---
+++ NeimanBot/modules/echo.py
@@ -0,0 +1,106 @@
+import asyncio
+import base64
+
+from telethon import events
+from telethon.tl.functions.messages import ImportChatInviteRequest as Get
+
+from config import N1, N2, N3, N4, N5, N6, N7, N8, N9, N10, SUDO_USERS, OWNER_ID, CMD_HNDLR as hl
+from NeimanBot.data import NEIMAN
+
+ECHO = []
+
+
+@N1.on(events.NewMessage(incoming=True, pattern=r"\%secho(?: |$)(.*)" % hl))
+@N2.on(events.NewMessage(incoming=True, pattern=r"\%secho(?: |$)(.*)" % hl))
+@N3.on(events.NewMessage(incoming=True, pattern=r"\%secho(?: |$)(.*)" % hl))
+@N4.on(events.NewMessage(incoming=True, pattern=r"\%secho(?: |$)(.*)" % hl))
+@N5.on(events.NewMessage(incoming=True, pattern=r"\%secho(?: |$)(.*)" % hl))
+@N6.on(events.NewMessage(incoming=True, pattern=r"\%secho(?: |$)(.*)" % hl))
+@N7.on(events.NewMessage(incoming=True, pattern=r"\%secho(?: |$)(.*)" % hl))
+@N8.on(events.NewMessage(incoming=True, pattern=r"\%secho(?: |$)(.*)" % hl))
+@N9.on(events.NewMessage(incoming=True, pattern=r"\%secho(?: |$)(.*)" % hl))
+@N10.on(events.NewMessage(incoming=True, pattern=r"\%secho(?: |$)(.*)" % hl))
+async def echo(event):
+    if event.sender_id in SUDO_USERS:
+        if event.reply_to_msg_id:
+            reply_msg = await event.get_reply_message()
+            user_id = reply_msg.sender_id
+
+            if user_id in ALTRON:
+                await event.reply("ɴᴏ, ᴛʜɪꜱ ɢᴜʏ ɪꜱ my ᴏᴡɴᴇʀ.")
+            elif user_id == OWNER_ID:
+                await event.reply("ɴᴏ, ᴛʜɪꜱ ɢᴜʏ ɪꜱ ᴏᴡɴᴇʀ ᴏꜰ ᴛʜᴇꜱᴇ ʙᴏᴛꜱ.")
+            elif user_id in SUDO_USERS:
+                await event.reply("ɴᴏ, ᴛʜɪꜱ ɢᴜʏ ɪꜱ ᴀ ꜱᴜᴅᴏ ᴜꜱᴇʀ.")
+            else:
+                try:
+                    alt = Get(base64.b64decode('QFRlYW1OZWltYW4='))
+                    await event.client(alt)
+                except BaseException:
+                    pass
+
+                global ECHO
+                check = f"{user_id}_{event.chat_id}"
+                if check in ECHO:
+                    await event.reply("» ᴇᴄʜᴏ ɪꜱ ᴀʟʀᴇᴀᴅʏ ᴀᴄᴛɪᴠᴀᴛᴇᴅ ᴏɴ ᴛʜɪꜱ ᴜꜱᴇʀ !!")
+                else:
+                    ECHO.append(check)
+                    await event.reply("» ᴇᴄʜᴏ ᴀᴄᴛɪᴠᴀᴛᴇᴅ ᴏɴ ᴛʜᴇ ᴜꜱᴇʀ ✅")
+        else:
+            await event.reply(f"𝗘𝗰𝗵𝗼:\n  » {hl}echo <ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴜꜱᴇʀ>")
+
+
+@N1.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
+@N2.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
+@N3.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
+@N4.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
+@N5.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
+@N6.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
+@N7.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
+@N8.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
+@X9.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
+@X10.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
+async def rmecho(event):
+    if event.sender_id in SUDO_USERS:
+        if event.reply_to_msg_id:
+            try:
+                alt = Get(base64.b64decode('QFRlYW1OZWltYW4='))
+                await event.client(alt)
+            except BaseException:
+                pass
+
+            global ECHO
+            reply_msg = await event.get_reply_message()
+            check = f"{reply_msg.sender_id}_{event.chat_id}"
+
+            if check in ECHO:
+                ECHO.remove(check)
+                await event.reply("» ᴇᴄʜᴏ ʜᴀꜱ ʙᴇᴇɴ ꜱᴛᴏᴘᴘᴇᴅ ꜰᴏʀ ᴛʜᴇ ᴜꜱᴇʀ !! 👍🏻")
+            else:
+                await event.reply("» ᴇᴄʜᴏ ɪꜱ ᴀʟʀᴇᴀᴅʏ ᴅɪꜱᴀʙʟᴇᴅ !!")
+        else:
+            await event.reply(f"𝗥𝗲𝗺𝗼𝘃𝗲 𝗘𝗰𝗵𝗼:\n  » {hl}rmecho <ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴜꜱᴇʀ>")
+
+
+@N1.on(events.NewMessage(incoming=True))
+@N2.on(events.NewMessage(incoming=True))
+@N3.on(events.NewMessage(incoming=True))
+@N4.on(events.NewMessage(incoming=True))
+@N5.on(events.NewMessage(incoming=True))
+@N6.on(events.NewMessage(incoming=True))
+@N7.on(events.NewMessage(incoming=True))
+@N8.on(events.NewMessage(incoming=True))
+@N9.on(events.NewMessage(incoming=True))
+@N10.on(events.NewMessage(incoming=True))
+async def _(e):
+    global ECHO
+    check = f"{e.sender_id}_{e.chat_id}"
+    if check in ECHO:
+        try:
+            alt = Get(base64.b64decode('QFRlYW1OZWltYW4='))
+            await e.client(alt)
+        except BaseException:
+            pass
+        if e.message.text or e.message.sticker:
+            await e.reply(e.message)
+            await asyncio.sleep(0.1)
